fb18e689dbf8e93b358d6a1b19d02f39d2d06e09
Mardoniosc <mardonio@live.com>
2020-10-28 10:49:30 -0300

    Adicionando lista de servicos

--- src/pages/servicos/servicos.ts
+++ src/pages/servicos/servicos.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ServicosPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
+export interface Servico {
+  id: number,
+  nome: string,
+}
 @IonicPage()
 @Component({
   selector: 'page-servicos',
   templateUrl: 'servicos.html',
 })
 export class ServicosPage {
+
+  servicos: Array<Servico> = [
+    {id: 1, nome: 'Elétrico'},
+    {id: 2, nome: 'Hidráulico'},
+    {id: 3, nome: 'Transporte'},
+    {id: 4, nome: 'Informática'},
+    {id: 5, nome: 'Marcenaria'},
+  ]
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
