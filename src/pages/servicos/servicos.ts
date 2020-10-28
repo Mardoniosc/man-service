23bf011dc5a3056e95e5e7286ab70c639ff4717a
Mardoniosc <mardonio@live.com>
2020-10-28 10:52:10 -0300

    Adicionando lista de servicos

--- src/pages/servicos/servicos.ts
+++ src/pages/servicos/servicos.ts
@@ -1,30 +1,44 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { Servico } from "../../shared";
 
-export interface Servico {
-  id: number,
-  nome: string,
-}
 @IonicPage()
 @Component({
-  selector: 'page-servicos',
-  templateUrl: 'servicos.html',
+  selector: "page-servicos",
+  templateUrl: "servicos.html",
 })
 export class ServicosPage {
-
   servicos: Array<Servico> = [
-    {id: 1, nome: 'Elétrico'},
-    {id: 2, nome: 'Hidráulico'},
-    {id: 3, nome: 'Transporte'},
-    {id: 4, nome: 'Informática'},
-    {id: 5, nome: 'Marcenaria'},
-  ]
+    { id: 1, nome: "Elétrico", telefone: "5561984137835", mensagem: "Bom dia" },
+    {
+      id: 2,
+      nome: "Hidráulico",
+      telefone: "5561984137835",
+      mensagem: "Bom dia",
+    },
+    {
+      id: 3,
+      nome: "Transporte",
+      telefone: "5561984137835",
+      mensagem: "Bom dia",
+    },
+    {
+      id: 4,
+      nome: "Informática",
+      telefone: "5561984137835",
+      mensagem: "Bom dia",
+    },
+    {
+      id: 5,
+      nome: "Marcenaria",
+      telefone: "5561984137835",
+      mensagem: "Bom dia",
+    },
+  ];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ServicosPage');
+    console.log("ionViewDidLoad ServicosPage");
   }
-
 }
